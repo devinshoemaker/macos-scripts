45da5755f3d19082bf141eec3927ffa1f268832e
Devin Shoemaker <devinshoe@gmail.com>
2018-02-07 21:18:19 -0600

    Install Homebrew and Homebrew Cask

---
+++ brew.sh
@@ -0,0 +1,7 @@
+# Install Homebrew on macOS
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Homebrew Cask
+brew tap caskroom/cask
