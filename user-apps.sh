2799da0a317819233f50fe32bb2a6f5207c22d63
Devin Shoemaker <devinshoe@gmail.com>
2018-02-07 21:45:30 -0600

    Install user applications

---
+++ user-apps.sh
@@ -0,0 +1,3 @@
+# Install User Applications
+
+brew cask install android-file-transfer firefox google-chrome marshallofsound-google-play-music-player slack spectacle transmission virtualbox virtualbox-extension-pack vlc
