449b1a18f8651d3b8465317233c4f3081def1cbc
Devin Shoemaker <devinshoe@gmail.com>
2018-02-11 11:45:23 -0600

    Node.js installation script

---
+++ nodejs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo '==> Checking if Homebrew is installed...'
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    echo '==> Homebrew not found, installing now.'
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo '==> Homebrew found, updating packages.'
+    brew update
+fi
+
+# Install NVM
+brew install nvm
+
+# Allow NVM without restarting
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
+echo '. "/usr/local/opt/nvm/nvm.sh"' >> ~/.bash_profile
