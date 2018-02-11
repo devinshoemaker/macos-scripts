f341af5ffaefa9891646464cfc7ed08f31b4b8f5
Devin Shoemaker <devinshoe@gmail.com>
2018-02-11 08:36:07 -0600

    fts: Add prompt for TunnelBlick install

--- fts.sh
+++ fts.sh
@@ -1,3 +1,9 @@
 # Install FTS Packages
 
-brew cask install tunnelblick
+echo '==> Install TunnelBlick VPN client? (Y/n)'
+read INSTALL_MACOS_UPDATES
+if [ "$INSTALL_MACOS_UPDATES" = 'y' ] || [ "$INSTALL_MACOS_UPDATES" = 'yes' ] || [ "$INSTALL_MACOS_UPDATES" = '' ]; then
+    brew cask install tunnelblick
+else
+    echo '==> Skipping TunnelBlick.'
+fi
