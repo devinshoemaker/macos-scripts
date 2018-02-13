2e59e4bc34ad422fc3026f6cb9a2848715128db2
Devin Shoemaker <devinshoe@gmail.com>
2018-02-13 12:21:42 -0600

    Delete unused scripts

--- development-tools.sh
+++
@@ -1,3 +0,0 @@
-# Install Development Apps and Tools
-
-brew cask install docker gitkraken intellij-idea iterm2 postman visual-studio-code
--- user-apps.sh
+++
@@ -1,3 +0,0 @@
-# Install User Applications
-
-brew cask install android-file-transfer firefox google-chrome marshallofsound-google-play-music-player slack spectacle transmission virtualbox virtualbox-extension-pack vlc
