a3d97bfe59db1134615242994be64d744eb0dba8
Devin Shoemaker <devinshoe@gmail.com>
2018-02-13 10:12:44 -0600

    nodejs: Install latest Node.js with NVM

--- nodejs.sh
+++ nodejs.sh
@@ -20,3 +20,6 @@ export NVM_DIR="$HOME/.nvm"
 
 echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
 echo '. "/usr/local/opt/nvm/nvm.sh"' >> ~/.bash_profile
+
+# Install latest Node.js with NVM
+nvm install node
