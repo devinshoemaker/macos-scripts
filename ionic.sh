0b7c8744466e0a9eeb9619983f3458e5ca0fc8ea
Devin Shoemaker <devinshoe@gmail.com>
2018-08-25 19:41:05 -0500

    ionic: update the user's cached credentials

--- ionic.sh
+++ ionic.sh
@@ -5,6 +5,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Update the user's cached credentials, authenticating the user if necessary
+sudo -v
+
 if ! command -v 'npm' >/dev/null 2>&1 ; then
     echo '==> NPM not installed. Installing Node.js now...'
     ./nodejs.sh
