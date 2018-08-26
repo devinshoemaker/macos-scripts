5d4b4b7018b0c809b9fa89090b59fcec1c2b782e
Devin Shoemaker <devinshoe@gmail.com>
2018-08-25 19:40:15 -0500

    ionic: check if NPM is installed

--- ionic.sh
+++ ionic.sh
@@ -2,4 +2,15 @@
 
 # macOS - Install Ionic
 
-npm install -g ionic cordova
+if ! command -v 'npm' >/dev/null 2>&1 ; then
+    echo '==> NPM not installed. Installing Node.js now...'
+    ./nodejs.sh
+fi
+
+if command -v 'npm' >/dev/null 2>&1 ; then
+    echo '==> Installing Angular CLI...'
+    npm install -g ionic cordova
+else
+    echo '==> NPM not installed. Aborting...'
+    exit 1
+fi
