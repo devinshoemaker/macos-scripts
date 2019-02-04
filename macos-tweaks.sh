08d21e34f2367026d1562b38ed8e58b6dbdbe4b6
Devin Shoemaker <devinshoe@gmail.com>
2019-02-04 08:23:14 -0600

    macos-tweaks: Disable spell check in Messages application

--- macos-tweaks.sh
+++ macos-tweaks.sh
@@ -71,10 +71,24 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 defaults write com.apple.dock autohide -bool true
 
 ###############################################################################
+# Messages                                                                    #
+###############################################################################
+
+# Disable automatic emoji substitution (i.e. use plain text smileys)
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
+
+# Disable smart quotes as it’s annoying for messages that contain code
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+
+# Disable continuous spell checking
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+
+###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 for app in "Dock" \
-	"Finder"; do
+	"Finder" \
+	"Messages"; do
 	killall "${app}" &> /dev/null
 done
 
