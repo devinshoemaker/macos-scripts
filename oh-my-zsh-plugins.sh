b2adde5d93be221d62791db4c233ad833acfae92
Devin Shoemaker <devinshoe@gmail.com>
2019-06-18 11:05:24 -0500

    oh-my-zsh-plugins: fix `~/.zshrc` check

--- oh-my-zsh-plugins.sh
+++ oh-my-zsh-plugins.sh
@@ -8,8 +8,10 @@ set -e
 # Store a local variable of the scripts current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ -f "~/.zshrc" ]]; then
+if [[ ! -f "~/.zshrc" ]]; then
+    echo "==> Configuring Oh My Zsh plugins..."
     sed -i -e 's/plugins=(git)/plugins=(git mvn nvm z)/g' ~/.zshrc
+    echo "==> Oh My Zsh plugins configured."
 else
     echo "==> .zshrc not found, Oh My Zsh possibly not installed. Aborting."
     exit 1
