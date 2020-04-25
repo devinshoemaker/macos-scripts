ae011a461af528a48fcc5c22a0ad064a84e716c3
Devin Shoemaker <devinshoe@gmail.com>
2020-04-25 18:47:42 -0500

    nodejs: install NVM with official script to fix zsh plugin

--- nodejs.sh
+++ nodejs.sh
@@ -12,40 +12,19 @@ sudo -v
 # Store a local variable of the scripts current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if ! command -v "brew" > /dev/null 2>&1; then
-    echo "==> Homebrew not found. Installing now..."
-    ${DIR}/brew.sh
-fi
+echo "==> Installing Node Version Manager..."
 
-if command -v "brew" > /dev/null 2>&1; then
-    echo "==> Installing Node Version Manager..."
+# Install Node Version Manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-    # Install Node Version Manager
-    brew install nvm
+# Allow NVM without restarting
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
 
-    # Allow NVM without restarting
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+echo "==> Node Version Manager installed."
 
-    # Allow NVM after restarting
-    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
-    echo '[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"' >> ~/.bash_profile
-    echo '[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"' >> ~/.bash_profile
-    echo '\n' >> ~/.bash_profile
-
-    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshenv
-    echo '[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"' >> ~/.zshenv
-    echo '[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"' >> ~/.zshenv
-    echo '\n' >> ~/.zshenv
-
-    echo "==> Node Version Manager installed."
-
-    # Install latest Node.js with NVM
-    echo "==> Installing latest Node.js LTS..."
-    nvm install --lts
-    echo "==> Latest Node.js LTS installed."
-else
-    echo "==> Homebrew not found. Aborting..."
-    exit 1
-fi
+# Install latest Node.js with NVM
+echo "==> Installing latest Node.js LTS..."
+nvm install --lts
+echo "==> Latest Node.js LTS installed."
